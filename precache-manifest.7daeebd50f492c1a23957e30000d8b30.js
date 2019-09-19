40a6d426f45e78d16cb049aa6860a230a4dcfd17
Amandeep Panesar <amandeep.s.panesar@gmail.com>
2019-09-18 22:00:16 -0400

    deploy

---
+++ precache-manifest.7daeebd50f492c1a23957e30000d8b30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba8ba1311a1893602daacb9cb4f2a0ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02b38c61ab5b5cebfebc",
+    "url": "/static/css/main.743b413d.chunk.css"
+  },
+  {
+    "revision": "073ab57eb9490370d2b4",
+    "url": "/static/js/2.a37d630b.chunk.js"
+  },
+  {
+    "revision": "02b38c61ab5b5cebfebc",
+    "url": "/static/js/main.c79db298.chunk.js"
+  },
+  {
+    "revision": "608567b3a286af875d2a",
+    "url": "/static/js/runtime~main.1dc4db23.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CarFinds-FrontEnd/precache-manifest.5a202669bdd8770306b00c813fb9d83f.js"
+  "/precache-manifest.7daeebd50f492c1a23957e30000d8b30.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/CarFinds-FrontEnd/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
