0eb14d4c3ab245477a4ccd19bc3446c86396cb46
Amandeep Panesar <amandeep.s.panesar@gmail.com>
2019-09-18 21:40:46 -0400

    deploy

---
+++ precache-manifest.eb06e6dd20abb797b8962fa2389c0f50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "996067c49fb87996889624c6c2493d1c",
+    "url": "/CarFinds-FrontEnd/index.html"
+  },
+  {
+    "revision": "e55fb808658629703b21",
+    "url": "/CarFinds-FrontEnd/static/css/main.743b413d.chunk.css"
+  },
+  {
+    "revision": "9bb3286691f6ddad8060",
+    "url": "/CarFinds-FrontEnd/static/js/2.5d75c983.chunk.js"
+  },
+  {
+    "revision": "e55fb808658629703b21",
+    "url": "/CarFinds-FrontEnd/static/js/main.5ce9dbf7.chunk.js"
+  },
+  {
+    "revision": "8e9c40d3fb5e751fc6ae",
+    "url": "/CarFinds-FrontEnd/static/js/runtime~main.6a89adbf.js"
+  }
+]);
--- src/components/Pagination.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Pagination } from 'semantic-ui-react'
-function CarCards(props) {
-    return(
-        <Pagination onPageChange={(e,{activePage}) => props.callbackFromParent(activePage)} defaultActivePage={props.page} totalPages={props.count} />
-    )
-}
-export default CarCards
