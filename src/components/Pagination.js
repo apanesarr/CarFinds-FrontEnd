88de6b6aa3426832e036a2115aeac08f9c397b89
Amandeep Panesar <amandeep.s.panesar@gmail.com>
2019-09-26 05:05:29 -0400

    Make Paginate more mobile friendly

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import { Pagination } from 'semantic-ui-react'
+import { Pagination,Responsive } from 'semantic-ui-react'
 function CarCards(props) {
     return(
-        <Pagination onPageChange={(e,{activePage}) => props.callbackFromParent(activePage)} defaultActivePage={props.page} totalPages={props.count} />
+        <div>
+            <Responsive maxWidth={560}>
+                <Pagination  boundaryRange={0} onPageChange={(e,{activePage}) => props.callbackFromParent(activePage)} defaultActivePage={props.page} ellipsisItem={null} firstItem={null} lastItem={null} siblingRange={1} totalPages={props.count} />
+            </Responsive>
+            <Responsive minWidth = {561}>
+                <Pagination onPageChange={(e,{activePage}) => props.callbackFromParent(activePage)} defaultActivePage={props.page} totalPages={props.count} />
+             </Responsive>
+        </div> 
     )
 }
 export default CarCards
